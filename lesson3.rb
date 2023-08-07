a8ec6c8d0c379c0c2a107d743b87197a88f80748
saitohanta <osyogatu17@icloud.com>
2023-08-07 08:13:08 +0000

    [Add]

--- lesson3.rb
+++ lesson3.rb
@@ -30,3 +30,19 @@ class Car
 end
 
 Car.turn("右")
+
+
+class Car
+  def run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+class Bus < Car
+  def run(distance)
+    super
+    puts "30人を乗せて、走っています。"  end
+end
+
+bus = Bus.new
+bus.run(5)
